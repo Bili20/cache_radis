e9caf23290eab1369cf4225054d5db8c34bdeb78
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-09-20 14:51:01 -0300

    redis

--- src/animals/repository/animalsRepo.ts
+++ src/animals/repository/animalsRepo.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { createAnimalDTO } from '../models/dto/createAnimals.dto';
 import { IAnimalsRepo } from '../models/interface/animalsRepo.interface';
 import { Animal } from '../models/schema/animals.schema';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AnimalRepo implements IAnimalsRepo {
   constructor(
     @InjectModel(Animal.name) private readonly animalsSchema: Model<Animal>,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
   ) {}
 
   async create(param: createAnimalDTO): Promise<Animal> {
@@ -18,6 +21,13 @@ export class AnimalRepo implements IAnimalsRepo {
   }
 
   async findOne(id: string): Promise<Animal> {
-    return await this.animalsSchema.findOne({ _id: id }).exec();
+    const cachedData = await this.cacheService.get<Animal>(id);
+    if (cachedData) {
+      return cachedData;
+    }
+    const animal = await this.animalsSchema.findOne({ _id: id }).exec();
+    await this.cacheService.set(id, animal);
+
+    return animal;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AnimalsModule } from './animals/animals.module';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://root:example@localhost:27017'),
-    CacheModule.register(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 60000,
+    }),
     AnimalsModule,
   ],
 })
